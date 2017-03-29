d68607d05919cd279dd7091d5448033e62a255f1
Kriyoni <chrioniespano1991@gmail.com>
2017-03-29 21:54:54 +0800

    Testing
    Testing purposes

---
+++ Portfolio ko/index.php
@@ -0,0 +1,38 @@
+
+
+<html>
+<head>
+	<title>CCE</title>
+	<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+	<link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+<header>
+	<div class="col-3">
+		<img  id="left-speaker" class="speaker-size" src="images/speaker-desig.png" alt="Why?">
+	</div> 
+	
+	<div  id="main-body" class="col-3">
+			<div id="nav-bar">
+			<ul>
+				<li><a href="#">About Me</a></li>
+				<li id="push-to-right"><a href="#">My Gallery</a></li>
+				<li id="push-to-right"><a href="#">Community</a></li>
+			</ul>
+		</div>
+	</div>
+
+	<div id="right-speaker" class="col-3">
+	<img  id="right-speaker" class="speaker-size" src="images/speaker-desig.png" alt="Why?">
+	</div>
+</header>
+	
+</body>
+</html>
+<!-- <img  id="right-speaker" class="speaker-size" src="images/speaker-desig.png" alt="Why?"> -->
+<!-- <div class="box-enter">
+	Hello Guest,
+		<div class="enter-new-page">
+		Please Enter
+		</div>
+	</div> -->
